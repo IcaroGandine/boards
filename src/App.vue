cf3dabc9b48de455fc452fcc19bffe958fba4975
Icaro Weiler <icaro.weiler@codenapp.com>
2021-06-18 03:44:59 -0300

    add bootstrap, create initial cards, update columns

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col" style="borderRight: 1px solid">
+      <div class="col-title"><h1>TO DO</h1></div>
+      <div class="col-content"><TaskCard v-bind:task="task" /></div>
+    </div>
+    <div class="col" style="borderRight: 1px solid">
+      <div class="col-title"><h1>DOING</h1></div>
+
+      <div class="col-content"><TaskCard v-bind:task="task" /></div>
+    </div>
+    <div class="col">
+      <div class="col-title"><h1>DONE</h1></div>
+      <div class="col-content"><TaskCard v-bind:task="task" /></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import TaskCard from "./components/TaskCard.vue";
+
+var task = {
+  header: "Header Task",
+
+  text: "Lorem Ipsum Dolor Amet",
+};
 
 export default {
-  name: 'App',
+  name: "App",
+  data: () => ({
+    task: task,
+  }),
   components: {
-    HelloWorld
-  }
-}
+    TaskCard,
+  },
+};
 </script>
 
 <style>
@@ -22,5 +45,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.col-content {
+  display: flex;
+  justify-content: center;
+}
+
+.row {
+  height: 100%;
 }
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      {{ task.header }}
+    </div>
+    <div class="card-body">
+      <p class="card-text">
+        {{ task.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskCard",
+  props: ["task"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  background: gray;
+  border: 1px solid;
+  width: 250px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.card-header {
+  font-weight: 700;
+  background: lightgray;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-body {
+  color: white;
+}
+</style>
